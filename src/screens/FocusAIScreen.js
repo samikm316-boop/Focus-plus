22903db05db765cd1cf1969dd828e916e9114a57
samikm316-boop <samikm316@gmail.com>
2026-05-13 23:23:21 +0530

    Create FocusAIScreen.js

---
+++ src/screens/FocusAIScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function FocusAIScreen() {
+  return (
+    <View>
+      <Text>Focus AI Screen</Text>
+    </View>
+  );
+}
