22df098677e2af83258c3d612c392c72da2e1b82
samikm316-boop <samikm316@gmail.com>
2026-05-14 19:27:53 +0530

    Refactor App component to use ThemeProvider and RootNavigator

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,13 @@
-import { useState } from "react";
+import React from "react";
 
-import HomeScreen from "./screens/HomeScreen";
-import StudyScreen from "./screens/StudyScreen";
-import FocusAIScreen from "./screens/FocusAIScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import SettingsScreen from "./screens/SettingsScreen";
+import { ThemeProvider } from "./context/ThemeContext";
 
-import Sidebar from "./components/Sidebar";
+import RootNavigator from "./navigation/RootNavigator";
 
-function App() {
-  const [page, setPage] = useState("home");
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const renderPage = () => {
-    switch (page) {
-      case "study":
-        return <StudyScreen setSidebarOpen={setSidebarOpen} />;
-
-      case "ai":
-        return <FocusAIScreen setSidebarOpen={setSidebarOpen} />;
-
-      case "profile":
-        return <ProfileScreen setSidebarOpen={setSidebarOpen} />;
-
-      case "settings":
-        return <SettingsScreen setSidebarOpen={setSidebarOpen} />;
-
-      default:
-        return <HomeScreen setSidebarOpen={setSidebarOpen} />;
-    }
-  };
-
+export default function App() {
   return (
-    <div className="app">
-      {sidebarOpen && (
-        <>
-          <div
-            className="sidebar-overlay"
-            onClick={() => setSidebarOpen(false)}
-          />
-
-          <Sidebar
-            setPage={setPage}
-            setSidebarOpen={setSidebarOpen}
-          />
-        </>
-      )}
-
-      {renderPage()}
-    </div>
+    <ThemeProvider>
+      <RootNavigator />
+    </ThemeProvider>
   );
 }
-
-export default App;
