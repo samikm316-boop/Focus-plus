0e0367dd13bf469984183dc80f3aee8ee40d428a
samikm316-boop <samikm316@gmail.com>
2026-05-14 12:15:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { ThemeProvider } from "./context/ThemeContext";
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      {/* your navigation + screens */}
+    </ThemeProvider>
+  );
+}
 
 export default function App() {
   return (
